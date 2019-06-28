d857ed10cca29e0135275a7db1ef1b862e553ac5
chenjunyao <chen13157379235@outlook.com>
2019-06-28 14:58:18 +0800

    update docker install script

--- linux/tools/docker.sh
+++ linux/tools/docker.sh
@@ -1,7 +1,27 @@
 #!/bin/bash
 set -xe
 
+uname -r
+
+# yum update
+
+# clean old version
+yum remove docker docker-common docker-selinux docker-engine -y
+
+# install yum-util
+yum install -y yum-utils device-mapper-persistent-data lvm2
+
+# add yum source
+yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+
+# check docker versions
+yum list docker-ce --showduplicates | sort -r
+
 # install docker
-yum install -y docker
-systemctl start docker.service
-systemctl enable docker.service
+yum install docker-ce -y
+
+systemctl start docker
+systemctl enable docker
+
+# see if installed
+docker version

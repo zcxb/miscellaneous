de9430730ecdc804301372897f33ea50ad431199
chenjunyao <chen13157379235@outlook.com>
2019-07-16 14:31:13 +0800

    add git init from local script

---
+++ git/git_init_from_local.sh
@@ -0,0 +1,15 @@
+git init
+
+git checkout -b develop
+
+git add .
+
+# git status
+
+git commit -m '项目注释'
+
+git remote add origin <REMOTE_REPOSITORY>
+
+git pull --rebase origin develop
+
+git push -u origin develop

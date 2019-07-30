b7ceb25fd82971ee2803785bbb76ad39a93f605a
chenjunyao <chen13157379235@outlook.com>
2019-07-30 19:56:11 +0800

    add docker deploy script

---
+++ linux/tools/docker-init.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+set -xe
+
+echo "This script will guide you to initialize a new docker container,"
+echo "Please use"
+echo ""
+echo "    cd <YOUR_RELEASE_PATH>"
+echo ""
+echo "to make sure you run this script in the right place."
+
+echo -n "Your app name: "
+read name
+echo -n "Inner port:"
+read inner_port
+echo -n "Outer port:"
+read outer_port
+echo -n "Restart with system reboot?(Y/n)"
+read b_restart
+
+if [ "$b_restart" == "" ]; then
+    b_restart=y
+fi
+
+cur_date="$(date +%m%d)"
+
+echo "Step.1 docker build"
+docker build -t $name:$cur_date .
+
+echo "Step.2 remove old container"
+docker ps --format "table {{.ID}}\t{{.Names}}" | grep $name | awk '{print $2}' | xargs docker rm -f
+
+echo "Step.3 run new container"
+if [ "$b_restart"x == "y"x -o "$b_restart"x == "Y"x ]; then
+    docker run --name $name_$cur_date -p $outer_port:$inner_port -d $name:$cur_date --restart always
+else
+    docker run --name $name_$cur_date -p $outer_port:$inner_port -d $name:$cur_date
+fi
+echo "Done!"
+exit 0

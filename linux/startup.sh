646a1196e21e5c07b69f30abbc6aa9a87004faed
chenjunyao <chen13157379235@outlook.com>
2019-08-08 16:42:02 +0800

    add sudoers and add user to group

--- linux/startup.sh
+++ linux/startup.sh
@@ -6,3 +6,15 @@ user_name="zcxb"
 
 useradd -d /home/${user_name} -m ${user_name}
 passwd ${user_name}
+
+# add to sudoers
+visudo
+$user_name ALL=(ALL) NOPASSWD: ALL
+
+# add user to group docker
+sudo usermod -aG docker $USER
+service docker restart # restart docker service
+
+# change ip
+cd /etc/sysconfig/network-scripts
+vim ifcfg-ens160

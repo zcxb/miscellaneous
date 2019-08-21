36d4b4d83c7804208c9e9ee945fbece19e7ce720
chenjunyao <chen13157379235@outlook.com>
2019-08-21 15:36:27 +0800

    add baget-server

--- nuget/nuget-server.sh
+++ nuget/nuget-server.sh
@@ -13,3 +13,11 @@ dotnet nuget push <PACKAGE_NAME>.<PACKAGE_VERSION>.nupkg -k <NUGET_API_KEY> -s h
 
 # delete nuget package from the server
 dotnet nuget delete <PACKAGE_NAME> <PACKAGE_VERSION> -k <NUGET_API_KEY> -s http://remote.nugetserver.com:8000/
+
+## nuget server above is not convenient enough
+
+# use baget
+docker run -d --restart always --name baget-server -p 8000:80 --env-file baget.env -v /home/dah
+uasurvey/temp/nuget/baget:/var/baget loicsharma/baget:latest
+
+ dotnet nuget push -s http://remote.nugetserver.com:8000/v3/index.json -k <NUGET_API_KEY> <PACKAGE_NAME>.<PACKAGE_VERSION>.nupkg

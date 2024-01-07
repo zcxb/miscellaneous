97a0c64a103ee9c7786207a760c7178794f45d78
zcxb <chen13157379235@outlook.com>
2024-01-07 10:52:33 +0800

    Update activate.ps1

--- windows/activate.ps1
+++ windows/activate.ps1
@@ -1,7 +1,11 @@
 # run as administrator
 
+# win 10 home edition
 slmgr /ipk NW6C2-QMPVW-D7KKK-3GKT6-VCFB2
-
 slmgr /skms kms.03k.org
-
 slmgr /ato
+
+# win 2022 datacenter edition
+slmgr -ipk WX4NM-KYWYW-QJJR4-XV3QB-6VM33
+slmgr -skms kms.0t.net.cn
+slmgr -ato
